4ff8353436b0e4052237767bcfa273c791567be7
Danilo Yakovliev <d.m.yakovliev@student.khai.edu>
2021-12-22 16:06:21 +0200

    Hangman 8 stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -15,55 +15,71 @@ public class hangman {
 
         String alphabet = "qwertyuiopasdfghjklzxcvbnm";
         var random = new SecureRandom();
-        String word = words.get(random.nextInt(words.size()));
-        char[] filler = new char[word.length()];
-        int i = 0;
-        while (i < word.length()) {
-            filler[i] = '-';
-            i++;
-        }
-
 
         System.out.println("HANGMAN");
         Scanner scanner = new Scanner(System.in);
-        int life = 8;
+        int life;
         int j;
-        while (life != 0) {
-            System.out.println(filler);
-            System.out.print("Input a letter: > ");
-            String answer = scanner.nextLine();
-            if (answer.length() > 1){
+        boolean isPlay = true;
+        while(isPlay){
+            System.out.println("Type \"play\" to play the game, \"exit\" to quit:");
+            String play = scanner.nextLine();
+
+            if (play.equals("exit")) {
+                isPlay = false;
+                break;
+            }
+            if (play.equals("play")){
+                String word = words.get(random.nextInt(words.size()));
+                char[] filler = new char[word.length()];
+                int i = 0;
+                while (i < word.length()) {
+                    filler[i] = '-';
+                    i++;
+                }
+                life = 8;
+
+                while (life != 0){
+
+                    System.out.println(filler);
+                    System.out.print("Input a letter: > ");
+                    String answer = scanner.nextLine();
+                    if (answer.length() > 1){
                 System.out.println("You should input a single letter.");
                 continue;
             }
-            char letter = answer.charAt(0);
-            if(!alphabet.contains(Character.toString(letter))){
-                System.out.println("Please enter a lowercase English letter.");
-                continue;
-            }
-            if (word.contains(letter + "")) {
+                    char letter = answer.charAt(0);
+                    if(!alphabet.contains(Character.toString(letter))){
+                        System.out.println("Please enter a lowercase English letter.");
+                        continue;
+                    }
+                    if (word.contains(letter + "")) {
 
-                for (j = 0; j < word.length(); j++) {
-                    if (filler[j] == letter) {
-                        System.out.println("You're already quessed this letter");
+                        for (j = 0; j < word.length(); j++) {
+                            if (filler[j] == letter) {
+                                System.out.println("You're already quessed this letter");
+                                break;
+                            }
+                            if (word.charAt(j) == letter) {
+                                filler[j] = letter;
+                            }
+                        }
+                    }
+                    else {
+                        System.out.println("That letter doesn't appear in the word");
+                        life--;
+                    }
+
+                    if (word.equals(String.valueOf(filler))) {
+                        System.out.println("You quessed the word!");
+                        System.out.println("You survived!");
                         break;
                     }
-                    if (word.charAt(j) == letter) {
-                        filler[j] = letter;
+                    if(life == 0){
+                        System.out.println("You lost!");
                     }
                 }
             }
-            else {
-                System.out.println("That letter doesn't appear in the word");
-                life--;
-            }
-
-                if (word.equals(String.valueOf(filler))) {
-                    System.out.println("You quessed the word!");
-                    System.out.println("You survived!");
-                    life = 0;
-            }
-
-            }
         }
     }
+}
