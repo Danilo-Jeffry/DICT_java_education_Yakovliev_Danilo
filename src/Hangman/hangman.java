8acd2414cbfedd6ab95ee4da99a732882fbdd4cb
Danilo Yakovliev <d.m.yakovliev@student.khai.edu>
2021-12-22 14:31:57 +0200

    Hangman 6 stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -16,8 +16,8 @@ public class hangman {
         String word = words.get(random.nextInt(words.size()));
         char[] filler = new char[word.length()];
         int i = 0;
-        while(i < word.length()){
-            filler[i]='-';
+        while (i < word.length()) {
+            filler[i] = '-';
             i++;
         }
 
@@ -26,28 +26,29 @@ public class hangman {
         Scanner scanner = new Scanner(System.in);
         int life = 8;
         int j;
-        while (life != 0){
+        while (life != 0) {
             System.out.println(filler);
             System.out.print("Input a letter: > ");
             char letter = scanner.nextLine().charAt(0);
-            if(word.contains(letter+"")) {
+            if (word.contains(letter + "")) {
                 for (j = 0; j < word.length(); j++) {
+                    if (filler[j] == letter) {
+                        System.out.println("No improvements");
+                        life--;
+                    }
                     if (word.charAt(j) == letter) {
                         filler[j] = letter;
                     }
                 }
-            }
-            else{
+            } else {
                 System.out.println("That letter doesn't appear in the word");
+                life--;
             }
-            life--;
-
-
-
+            if (word.equals(String.valueOf(filler))) {
+                System.out.println("You quessed the word!");
+                System.out.println("You survived!");
+                life = 0;
+            }
         }
-        System.out.println(filler);
-        System.out.println("""
-                Thanks for playing!
-                We'll see how well you did in the next stage""");
     }
 }
